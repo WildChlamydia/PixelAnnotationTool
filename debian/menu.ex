6fc05b7bb65527eec4cc6146510fdbc0e01c9a38
Wildchlamydia <undeadblow@gmail.com>
2018-08-31 17:59:59 +0300

    clean

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(pixelann):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="pixelann" command="/usr/bin/pixelann"
--- debian/pixelann.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pixelann package
-#
-0 4	* * *	root	[ -x /usr/bin/pixelann_maintenance ] && /usr/bin/pixelann_maintenance
